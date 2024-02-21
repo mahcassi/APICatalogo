2a231647fe60c8cecb68e0545094af8e2a17fc0d
Maria Eduarda <mariaed.cassiano@gmail.com>
2024-02-21 16:29:56 -0300

    Criada classe de Contexto e mapeamento das entidades

---
+++ APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context
+{
+    public class AppDbContext : DbContext
+    {
+        // essa parametro vai conter as opções de configuração que
+        // serão usadas para configurar o contexto do banco de dados (ex: string de conexão)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options) {}
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
