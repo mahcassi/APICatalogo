f1dd9967435e21e6737721464bf5a5f37ad5236d
Maria Eduarda <mariaed.cassiano@gmail.com>
2024-03-06 17:39:01 -0300

    método Get de produtos

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APICatalogo.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ProdutosController : ControllerBase
     {
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+
+            if (!produtos.Any()) 
+            {
+                return NotFound();
+            }
+            return produtos;
+        }
     }
 }
