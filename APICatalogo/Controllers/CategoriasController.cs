67fd03c8781d167459642ff8b7c0a9673e25810c
Maria Eduarda <mariaed.cassiano@gmail.com>
2024-03-11 21:12:43 -0300

    Adicionando AsNoTracking nas consultas

--- APICatalogo/Controllers/CategoriasController.cs
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -16,10 +16,16 @@ public class CategoriasController : ControllerBase
         _context = context;
     }
 
+    [HttpGet("produtos")]
+    public ActionResult<IEnumerable<Categoria>> GetCategoriasProdutos()
+    {
+        return _context.Categorias.Include(p => p.Produtos).Where(c => c.CategoriaId <=5).AsNoTracking().ToList();
+    }
+
     [HttpGet]
     public ActionResult<IEnumerable<Categoria>> Get()
     {
-        return _context.Categorias.ToList();
+        return _context.Categorias.AsNoTracking().ToList();
     }
 
     [HttpGet("{id:int}", Name = "ObterCategoria")]
