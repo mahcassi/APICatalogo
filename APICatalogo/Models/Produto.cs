91841bf0147751182fbfad90759ea11927e9d1b0
Maria Eduarda <mariaed.cassiano@gmail.com>
2024-03-16 19:03:17 -0300

    Criada validação personalizada para aceitar produto apenas com a primeira letra maiuscula

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using APICatalogo.Validations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
@@ -10,20 +11,24 @@ public class Produto
     [Key]
     public int ProdutoId { get; set; }
 
-    [Required]
-    [StringLength(80)]
+    [Required(ErrorMessage = "O nome é obrigatório")]
+    [StringLength(20, ErrorMessage = "O nome deve ter entre 5 e 20 caracteres", MinimumLength = 5)]
+    [PrimeiraLetraMaiusculaAttribute]
     public string? Nome { get; set; }
 
     [Required]
-    [StringLength(300)]
+    [StringLength(20, ErrorMessage = "A descrição deve ter no máximo {1} caracteres")]
     public string? Descricao { get; set; }
+
     [Required]
     [Column(TypeName = "decimal(10,2)")]
+    [Range(1, 10000, ErrorMessage = "O preço deve estar entre {1} e {2}")]
     public decimal Preco { get; set; }
 
     [Required]
-    [StringLength(300)]
+    [StringLength(300, MinimumLength = 10)]
     public string? ImagemUrl { get; set; }
+
     public float Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
     public int CategoriaId { get; set; }
