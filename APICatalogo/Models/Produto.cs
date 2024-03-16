d91506d60a0fc2ba6430766a66bd7162e9ca4187
Maria Eduarda <mariaed.cassiano@gmail.com>
2024-03-16 20:37:48 -0300

    Validação personalizada com IValidatableObject

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -6,14 +6,14 @@ using System.Text.Json.Serialization;
 namespace APICatalogo.Models;
 
 [Table("Produtos")]
-public class Produto
+public class Produto : IValidatableObject
 {
     [Key]
     public int ProdutoId { get; set; }
 
     [Required(ErrorMessage = "O nome é obrigatório")]
     [StringLength(20, ErrorMessage = "O nome deve ter entre 5 e 20 caracteres", MinimumLength = 5)]
-    [PrimeiraLetraMaiusculaAttribute]
+    //[PrimeiraLetraMaiusculaAttribute]
     public string? Nome { get; set; }
 
     [Required]
@@ -35,4 +35,22 @@ public class Produto
 
     [JsonIgnore]
     public Categoria? Categoria { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (!string.IsNullOrEmpty(this.Nome))
+        {
+            var primeiraLetra = this.Nome[0].ToString();
+            if (primeiraLetra != primeiraLetra.ToUpper())
+            {
+                // yield retorna cada elemento individualmente
+                yield return new ValidationResult("A primeira letra do produto deve ser maiuscula", new[] { nameof(this.Nome) });
+            }
+
+            if (this.Estoque <= 0)
+            {
+                yield return new ValidationResult("Estoque deve ser maior que zero", new[] { nameof(this.Estoque) });
+            }
+        }
+    }
 }
